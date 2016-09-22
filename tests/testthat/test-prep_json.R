0cbcb5b81d9030a8042bb1ad21627e701e6e3f03
Kris Sankaran <sankaran.kris@gmail.com>
2016-09-22 10:15:32 -0700

    Test for building edgelist from taxa matrix
    Case without any NAs.

--- tests/testthat/test-prep_json.R
+++ tests/testthat/test-prep_json.R
@@ -62,3 +62,22 @@ test_that("Fails when given wrong column names.", {
                    y = c("b", "c", "d"))
   expect_error(tree_json(el, "a"))
 })
+
+test_that("Builds basic edgelist.", {
+  taxa <- matrix(
+    c("1", "2", "1", "2", "1", "2", "1", "6"),
+    nrow = 4,
+    byrow = TRUE,
+    dimnames = list(c("3", "4", "5", "7"),
+                    c("depth_1", "depth_2"))
+  )
+
+  el <- data.frame(
+    parent = c("1", "1", "2", "2", "2", "6"),
+    child = c("2", "6", "3", "4", "5", "7")
+  )
+
+  expect_equal(taxa_edgelist(taxa), el)
+})
+
+test_that("Builds edgelist when NAs are present")
