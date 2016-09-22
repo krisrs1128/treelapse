e4832b67763d75923649e076ef107d3be7178bdd
Kris Sankaran <sankaran.kris@gmail.com>
2016-09-21 18:11:08 -0700

    Generic d3 utils
    (mainly needed for link id fun)

---
+++ inst/htmlwidgets/lib/doi/d3_utils.js
@@ -0,0 +1,34 @@
+/**
+* Helper functions for d3 visualizations
+**/
+
+function setup_background(elem, width, height, fill) {
+  var svg_elem = d3.select(elem)
+      .append("svg")
+      .attrs({
+	width: width,
+	height: height
+      });
+
+  var rect = d3.select("svg")
+      .append("rect")
+      .attrs({
+	id: "background_rect",
+	width: width,
+	height: height,
+	fill: fill
+      });
+
+}
+
+function setup_groups(svg) {
+  svg.append("g")
+    .attr("id", "links");
+
+  svg.append("g")
+    .attr("id", "nodes");
+}
+
+function link_id_fun(d) {
+  return d.source.data.name + "-" + d.target.data.name;
+}
