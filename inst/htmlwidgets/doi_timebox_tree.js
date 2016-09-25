098b3b8d41c63267a02d3db7a1a92352c0742748
Kris Sankaran <sankaran.kris@gmail.com>
2016-09-24 18:55:41 -0700

    timeboxes htmlwidgets js

---
+++ inst/htmlwidgets/doi_timebox_tree.js
@@ -0,0 +1,16 @@
+HTMLWidgets.widget({
+
+  name: 'doi_timebox_tree',
+  type: 'output',
+
+  factory: function(el, width, height) {
+    return {
+
+      renderValue: function(x) {
+	console.log("Nothing yet....")
+      },
+
+      resize: function(width, height) {}
+    };
+  }
+});
