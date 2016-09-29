0395860e2e981eec9e6826b319490ca3b51645cb
Kris Sankaran <sankaran.kris@gmail.com>
2016-09-29 15:43:10 -0700

    Use d3-scale-chromatic
    This gives a color scheme whose neighbors don't blend in.

--- inst/htmlwidgets/lib/doi/sankey_update.js
+++ inst/htmlwidgets/lib/doi/sankey_update.js
@@ -1,7 +1,6 @@
 // placeholder
 
 function draw_sankey(elem, width, height, values, tree, focus_node_id) {
-  setup_search(elem);
   setup_background(elem, width, height, "#F7F7F7");
   setup_groups(d3.select("svg"), ["links"]);
   sankey_update(
@@ -21,9 +20,13 @@ function sankey_update(width, height, values, tree, focus_node_id) {
   var doi_tree = new DoiTree(tree);
   doi_tree.set_doi();
 
+  var groups = d3.set(values.group).values();
   var scales = {
     "size": d3.scaleLinear()
       .domain([0, d3.max(values.value)])
       .range([3, 35]),
+    "fill": d3.scaleOrdinal(d3.schemeSet3)
+      .domain(groups)
   };
+
 }
