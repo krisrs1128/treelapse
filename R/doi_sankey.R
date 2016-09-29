615cbd3ade08d04ab8d79809a96722195a0be383
Kris Sankaran <sankaran.kris@gmail.com>
2016-09-28 23:03:19 -0700

    R wrapper for doi sankey htmlwidgets

---
+++ R/doi_sankey.R
@@ -0,0 +1,31 @@
+#' <Add Title>
+#'
+#' <Add Description>
+#' @import htmlwidgets
+#' @importFrom jsonlite toJSON
+#' @export
+doi_sankey <- function(values, edges, focus_node_id = NULL, root = NULL,
+                       width = NULL, height = NULL) {
+  if (is.null(focus_node_id)) {
+    focus_node_id  <- edges[1, 1]
+  }
+  if (is.null(root)) {
+    root  <- edges[1, 1]
+  }
+
+  # forward options using x
+  x <- list(
+    values,
+    toJSON(tree_json(edges, root)),
+    focus_node_id
+  )
+
+  # create widget
+  htmlwidgets::createWidget(
+    name = "doi_sankey",
+    x,
+    width = width,
+    height = height,
+    package = "treelapse"
+  )
+}
