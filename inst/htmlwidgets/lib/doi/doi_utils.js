32138fe5fd5c5aff67df9de9aa9a59d853296614
Kris Sankaran <sankaran.kris@gmail.com>
2016-09-29 11:07:12 -0700

    Use square root transform instead

--- inst/htmlwidgets/lib/doi/doi_utils.js
+++ inst/htmlwidgets/lib/doi/doi_utils.js
@@ -571,9 +571,11 @@ function doi_node_attrs(values, scales, tree_obj, search_str) {
 
 function doi_link_attrs(values, scales) {
   var attrs = link_attr_defaults();
+
   attrs.stroke = function(d) {
     return scales.fill(d.target.data.doi);
   };
+
   attrs["stroke-width"] = function(d) {
     var cur_values = get_matching_subarray(
       values.value,
@@ -588,6 +590,7 @@ function doi_link_attrs(values, scales) {
 
 function doi_highlight_link_attrs(values, scales, tree_obj, search_str) {
   var attrs = link_attr_defaults();
+
   attrs["stroke-width"] = function(d) {
     var cur_tree = tree_obj.get_subtree(d.target.data.name);
     if (!(search_str !== "" & cur_tree.contains_partial_match(search_str))) {
