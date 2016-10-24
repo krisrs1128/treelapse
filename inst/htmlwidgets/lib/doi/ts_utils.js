df3727b3156d50e08f1e93ae84e4ae182dbf095b
Kris Sankaran <sankaran.kris@gmail.com>
2016-10-24 12:29:06 -0700

    Rest of documentation for time / treeboxes

--- inst/htmlwidgets/lib/doi/ts_utils.js
+++ inst/htmlwidgets/lib/doi/ts_utils.js
@@ -1,3 +1,31 @@
+/*******************************************************************************
+* @fileoverview Functions for drawing and updating time and treebox displays.
+* @see_also ts_update.js
+*
+* @author kriss1@stanford.edu
+*******************************************************************************/
+
+/*******************************************************************************
+* Functions for drawing the static tree and time series in timebox trees
+*******************************************************************************/
+
+/**
+ * Return the scales required for positioning in time + treeboxes
+ *
+ * @param {object} values An object with three subarrays,
+ *       - value {array of float} The y values associated with Tree nodes.
+ *       - unit {array of string} The node names associated with values.
+ *     The i^th element in each of the three arrays correspond to the same
+ *     entity.
+ * @param {float} width The available width (in pixels) for the display, used
+ *     for calculating the range of x values.
+ * @param {float} height The available height (in pixels) for the display, used
+ *     for calculating the range of y values.
+ * @param {float} size_min The minimum size (in pixels) of any node.
+ * @param {float} size_max The maximum size (in pixels) of any node.
+ * @return {dictionary of d3-scale} A dictionary keyed by "x", "y", and "r"
+ *     giving scales for computing positions in the time + treeboxes.
+ **/
 function get_scales(values, width, height, size_min, size_max) {
   return {
     "x": d3.scaleLinear()
@@ -12,6 +40,29 @@ function get_scales(values, width, height, size_min, size_max) {
   };
 }
 
+/**
+ * Draw static TS associated with time / treeboxes
+ *
+ * @param elem {d3 selection} The html selection on which all the brushes to
+ *     check are located.
+ * @param {object} values An object with three subarrays,
+ *       - time {array of float} The times associated with Tree nodes.
+ *       - value {array of float} The y values associated with Tree nodes.
+ *       - unit {array of string} The node names associated with values.
+ *     The i^th element in each of the three arrays correspond to the same
+ *     entity.
+ * @param cur_lines {array of string} An array of IDs for the time series /
+ *     nodes that are currently being selected by either timeboxes or treeboxes.
+ *     This will be used to highlight those nodes in the tree (and mute the
+ *     rest).
+ * @param scales {dictionary of d3-scales} An object with keys "x" and "y"
+ *     giving scales from which x and y positions of the tree nodes are
+ *     calculated.
+ * @param mouseover_text {boolean} true or false depending on whether to display
+ *     mouseover information when a node is hovered over.
+ * @return null
+ * @side-effects Draws the static time series (svg-paths) on the elem.
+ **/
 function draw_ts(elem, values, cur_lines, scales, mouseover_text) {
   var units = d3.set(values.unit).values();
   var ts_selection = d3.select(elem)
@@ -92,6 +143,25 @@ function draw_ts(elem, values, cur_lines, scales, mouseover_text) {
 
 }
 
+/**
+ * Specify attribute functions for tree links in time + treeboxes
+ *
+ * @param {object} values An object with three subarrays,
+ *       - value {array of float} The y values associated with Tree nodes.
+ *       - unit {array of string} The node names associated with values.
+ *     The i^th element in each of the three arrays correspond to the same
+ *     entity.
+ * @param cur_lines {array of string} An array of IDs for the time series /
+ *     nodes that are currently being selected by either timeboxes or treeboxes.
+ *     This will be used to highlight those nodes in the tree (and mute the
+ *     rest).
+ * @param scales {dictionary of d3-scales} An object with keys "r"  giving
+ *     the scale from which the radius of the tree nodes are calculated / width
+ *     of tree edges.
+ * @return {dictionary of functions} A dictionary containing functions that can
+ *     be directly input to a d3 path selection's .attr() to give styling /
+ *     positioning for text on time + treeboxes tree nodes.
+ **/
 function timebox_link_attrs(values, cur_lines, scales) {
   var attr_funs = link_attr_defaults();
   attr_funs.stroke = "#F0F0F0";
@@ -108,6 +178,24 @@ function timebox_link_attrs(values, cur_lines, scales) {
   return attr_funs;
 }
 
+/**
+ * Specify attribute functions for tree nodes in time + treeboxes
+ *
+ * @param {object} values An object with three subarrays,
+ *       - value {array of float} The y values associated with Tree nodes.
+ *       - unit {array of string} The node names associated with values.
+ *     The i^th element in each of the three arrays correspond to the same
+ *     entity.
+ * @param cur_lines {array of string} An array of IDs for the time series /
+ *     nodes that are currently being selected by either timeboxes or treeboxes.
+ *     This will be used to highlight those nodes in the tree (and mute the
+ *     rest).
+ * @param scales {dictionary of d3-scales} An object with keys "r"  giving
+ *     the scale from which the radius of the tree nodes are calculated.
+ * @return {dictionary of functions} A dictionary containing functions that can
+ *     be directly input to a d3 circles selection's .attr() to give styling /
+ *     positioning for text on time + treeboxes tree nodes.
+ **/
 function timebox_node_attrs(values, cur_lines, scales) {
   var attr_funs = node_attr_defaults();
 
@@ -130,6 +218,32 @@ function timebox_node_attrs(values, cur_lines, scales) {
   return attr_funs;
 }
 
+/**
+ * Draw the static tree associated with a tree object
+ *
+ * @param elem {d3 selection} The html selection on which all the brushes to
+ *     check are located.
+ * @param {object} values An object with three subarrays,
+ *       - time {array of float} The times associated with Tree nodes.
+ *       - value {array of float} The y values associated with Tree nodes.
+ *       - unit {array of string} The node names associated with values.
+ *     The i^th element in each of the three arrays correspond to the same
+ *     entity.
+ * @param cur_lines {array of string} An array of IDs for the time series /
+ *     nodes that are currently being selected by either timeboxes or treeboxes.
+ *     This will be used to highlight those nodes in the tree (and mute the
+ *     rest).
+ * @param tree {Tree} A tree object (actually, a properly nested JSON would
+ *     suffice) on which we can call d3.hierarchy to compute the layout.
+ * @param scales {dictionary of d3-scales} An object with keys "x" and "y"
+ *     giving scales from which x and y positions of the tree nodes are
+ *     calculated.
+ * @param mouseover_text {boolean} true or false depending on whether to display
+ *     mouseover information when a node is hovered over.
+ * @return null
+ * @side-effects Draws the static tree structure (circles and paths between
+ *     them) on elem.
+ **/
 function draw_tree(elem, values, cur_lines, tree, scales, mouseover_text) {
   var hierarchy = d3.hierarchy(tree);
 
@@ -158,6 +272,7 @@ function draw_tree(elem, values, cur_lines, tree, scales, mouseover_text) {
     100
   );
 
+  // display names on mouseover
   if (mouseover_text) {
     d3.select(elem)
       .selectAll(".tree_node")
@@ -182,6 +297,23 @@ function draw_tree(elem, values, cur_lines, tree, scales, mouseover_text) {
   }
 }
 
+/*******************************************************************************
+* Functions required for doing timebox / treebox querying using brush extents
+*******************************************************************************/
+
+/**
+ * Reshape line data in form required for d3.svg.line()
+ *
+ * @param {object} values An object with three subarrays,
+ *       - time {array of float} The times associated with Tree nodes.
+ *       - value {array of float} The y values associated with Tree nodes.
+ *       - unit {array of string} The node names associated with values.
+ *     The i^th element in each of the three arrays correspond to the same
+ *     entity.
+ * @param cur_unit {string} The ID of the entity to extract the time series for.
+ * @return {array of objects} An with time / value pairs for each time series
+ *     line. For example, [{"time": 0, "value": 1}, ...]
+ **/
 function get_line_data(values, cur_unit) {
   var cur_times = get_matching_subarray(
     values.time,
@@ -200,29 +332,20 @@ function get_line_data(values, cur_unit) {
   });
 }
 
-function intersect(a, b) {
-  var t;
-  if (b.length > a.length) t = b, b = a, a = t; // indexOf to loop over shorter
-  return a.filter(function (e) {
-    if (b.indexOf(e) !== -1) return true;
-  });
-}
-
-function union(a, b) {
-  var obj = {};
-  for (var i = a.length - 1; i >= 0; --i)
-     obj[a[i]] = a[i];
-  for (var i = b.length - 1; i >= 0; --i)
-     obj[b[i]] = b[i];
-  var res = [];
-  for (var k in obj) {
-    if (obj.hasOwnProperty(k))  // <-- optional
-      res.push(obj[k]);
-  }
-  return res;
-}
-
+/**
+ * Focus on the brush with the specified id
+ *
+ * The "focused" brush is the only one that we can manipulate at a time.
+ * @param elem {d3 selection} The html selection on which all the brushes to
+ *     check are located.
+ * @param brush_ix {int} The index for the brush to refocus on.
+ * @return null
+ * @side-effects Updates the styling and pointer-events for each brush so that
+ *     the brush with id "brush-{brush-id}" is the focus.
+ **/
 function focus_brush(elem, brush_ix) {
+
+  // update pointer events / styling for each brush
   d3.select(elem)
     .selectAll(".brush")
     .attrs({
@@ -248,6 +371,8 @@ function focus_brush(elem, brush_ix) {
 	return 0.4;
       }
     });
+
+  // same updates, but for rectangle contained in brush
   d3.select(elem)
     .selectAll(".brush > rect")
     .attrs({
@@ -258,6 +383,15 @@ function focus_brush(elem, brush_ix) {
     });
 }
 
+/**
+ * Update which brush is focused (can be clicked / dragged)
+ *
+ * @param elem {d3 selection} The html selection on which all the brushes to
+ *     check are located.
+ * @return null
+ * @side-effects Cycles the brush with the top-pointer event (from the one that
+ *     is selected to whatever the next one that had been added is).
+ **/
 function change_focus(elem) {
   var brushes = d3.select(elem)
       .selectAll(".brush").nodes();
@@ -274,6 +408,21 @@ function change_focus(elem) {
   focus_brush(elem, brush_ix);
 }
 
+/**
+ * Get the extent of a brush
+ *
+ * @param brushes {array of d3-brush} An array containing all the d3-brushes on
+ *     the display.
+ * @param scales {dictionary of d3-scales} An object with keys "x" and "y"
+ *     giving scales from which x and y positions of the brushes are calculated.
+ * @return box_extent {Object} An object specifying the bounds for
+ *     nodes which we should return as "in the box". It must have the keys,
+ *       - x_min {float} The minimum x-value for the node to in order for it to
+ *             be returned.
+ *       - x_max {float} Same, for maximum x-value.
+ *       - y_min {float} Same, for minimum y-value.
+ *       - y_max {float} Same, for maximum y-value.
+ **/
 function get_box_extent(brush, scales) {
   var box_extent = d3.brushSelection(brush);
   return {
@@ -393,8 +542,6 @@ function lines_in_box(line_data, box_extent) {
  *     check are located.
  * @param brushes {array of d3-brush} An array containing all the d3-brushes on
  *     the display.
- * @units {array} This should be empty. It is required so that we can use
- *     new_brush() as a wrapper for both the union and intersection methods.
  * @return units {array of strings} The ids for tree nodes contained in any of
  *     the specified brushes.
  **/
@@ -404,6 +551,7 @@ function brush_nodes_union(elem, brushes) {
     "y": d3.scaleLinear()
   };
 
+  var units = [];
   for (var i = 0; i < brushes.length; i++) {
     var box_extent = get_box_extent(brushes[i], scales);
     units = union(
@@ -443,4 +591,45 @@ function nodes_in_box(elem, box_extent) {
     function(d) { return d.id; }
   );
   return node_ids;
+}
+
+
+/*******************************************************************************
+* Miscellaneous helpers
+*******************************************************************************/
+
+/**
+ * Get the intersection of two arrays
+ *
+ * @param a {array} One of the arrays to intersect.
+ * @param b {array} One of the arrays to intersect.
+ * @return {array} An array containing elements that are in both a and b.
+ **/
+function intersect(a, b) {
+  var t;
+  if (b.length > a.length) t = b, b = a, a = t; // indexOf to loop over shorter
+  return a.filter(function (e) {
+    if (b.indexOf(e) !== -1) return true;
+  });
+}
+
+/**
+ * Get the union of two arrays
+ *
+ * @param a {array} One of the arrays to union.
+ * @param b {array} One of the arrays to union.
+ * @return {array} An array containing any elements that are in either a or b.
+ **/
+function union(a, b) {
+  var obj = {};
+  for (var i = a.length - 1; i >= 0; --i)
+     obj[a[i]] = a[i];
+  for (var i = b.length - 1; i >= 0; --i)
+     obj[b[i]] = b[i];
+  var res = [];
+  for (var k in obj) {
+    if (obj.hasOwnProperty(k))  // <-- optional
+      res.push(obj[k]);
+  }
+  return res;
 }
