526defd76cd5cb924d652aa7e77812538ac70e73
Kris Sankaran <sankaran.kris@gmail.com>
2016-11-15 12:04:37 -0800

    Simplify code for antibiotic vignette
    Use the tree_funs_multi wrapper

--- inst/doc/antibiotic_code.R
+++ inst/doc/antibiotic_code.R
@@ -47,51 +47,19 @@ for (parent in c("Erysipelotrichi_Erysipelotrichales", "Lachnospiraceae", "Rumin
 edges <- taxa_edgelist(taxa)
 
 ## ---- aggregate-tips ----
-times <- unique(mapping$time)
+
 subjects <- unique(mapping$ind)
-values <- data.frame(
-  subject = character(0),
-  time = numeric(0),
-  type = character(0),
-  unit = character(0),
-  value = numeric(0)
-)
-
-for (i in seq_along(times)) {
-  for (j in seq_along(subjects)) {
-    if (i %% 10 == 0) {
-      cat(sprintf(
-        "Computing tree stats for subject %s at time %f \n",
-        subjects[j],
-        times[i]
-      ))
-    }
-
-    cur_ix <- mapping$time == times[i] & mapping$ind == subjects[j]
-    if (!any(cur_ix)) next
-
-    cur_tips <- setNames(tip_values[cur_ix, ], colnames(tip_values))
-    cur_sums <- tree_sum(edges, cur_tips)
-    values <- rbind(
+values <- list()
+for (i in seq_along(subjects)) {
+  cur_ix  <- mapping$ind == subjects[j]
+  for (fun in c("sum", "mean")) {
+    tree_fun <- get(sprintf("tree_%s", fun))
+    values <- c(
       values,
-      data.frame(
-        "subject" = subjects[j],
-        "time" = times[i],
-        "type" = "sum",
-        "unit" = names(cur_sums),
-        "value" = cur_sums
-      )
-    )
-
-    cur_means <- tree_mean(edges, cur_tips)
-    values <- rbind(
-      values,
-      data.frame(
-        "subject" = subjects[j],
-        "time" = times[i],
-        "type" = "mean",
-        "unit" = names(cur_means),
-        "value" = cur_means
+      data.table(
+        "subject" = subjects[i],
+        "type" = fun,
+        tree_fun_multi(edges, t(tip_values[cur_ix, ]), tree_fun)
       )
     )
   }
